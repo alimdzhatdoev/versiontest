48ce92818378c8c05a3fd81c43c3419d5517c689
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2023-12-08 16:52:28 +0300

    update

--- check_updates.php
+++ check_updates.php
@@ -1,56 +1,64 @@
 <?php
 
 if (isset($_POST['check_updates'])) {
-    $username = 'alimdzhatdoev';
-    $repo = 'versiontest';
-    $path = 'package.json';
-    
-    // Сформируйте URL для запроса к GitHub API
-    $url = "https://api.github.com/repos/$username/$repo/contents/$path";
-    
-    // Если ваш репозиторий закрыт, убедитесь, что у вас есть токен авторизации
-    // $token = 'ваш-токен';
-    
-    // Формируем заголовки для запроса
-    $headers = [
+    // Путь к вашему package.json файлу на сервере
+    $localPackageJsonPath = __DIR__ . '/package.json';
+
+    // GitHub репозиторий
+    $githubUsername = 'alimdzhatdoev';
+    $githubRepo = 'versiontest';
+    $githubPath = 'package.json';
+
+    // GitHub API URL
+    $githubUrl = "https://api.github.com/repos/$githubUsername/$githubRepo/contents/$githubPath";
+
+    // GitHub API заголовки
+    $githubHeaders = [
         'User-Agent: Your-App-Name', // Замените на имя вашего приложения
         // Если используете токен, добавьте заголовок авторизации
         // 'Authorization: Bearer ' . $token,
     ];
-    
-    // Отправляем запрос и получаем результат
-    $options = [
+
+    // Отправляем запрос к GitHub API
+    $githubOptions = [
         'http' => [
-            'header' => implode("\r\n", $headers),
+            'header' => implode("\r\n", $githubHeaders),
             'method' => 'GET',
         ],
     ];
-    
-    $context = stream_context_create($options);
-    $response = file_get_contents($url, false, $context);
-    
+
+    $githubContext = stream_context_create($githubOptions);
+    $githubResponse = file_get_contents($githubUrl, false, $githubContext);
+
     // Парсим JSON-ответ
-    $data = json_decode($response, true);
-    
-    // Извлекаем версию из package.json
-    if ($data && isset($data['content'])) {
-        $content = base64_decode($data['content']);
-        $packageJson = json_decode($content, true);
-    
-        if ($packageJson && isset($packageJson['version'])) {
-            $version = $packageJson['version'];
-            echo 'Версия проекта: ' . $version;
+    $githubData = json_decode($githubResponse, true);
+
+    // Извлекаем версию из package.json на GitHub
+    if ($githubData && isset($githubData['content'])) {
+        $githubContent = base64_decode($githubData['content']);
+        $githubPackageJson = json_decode($githubContent, true);
+
+        // Извлекаем версию из package.json на сервере
+        $localContent = file_get_contents($localPackageJsonPath);
+        $localPackageJson = json_decode($localContent, true);
+
+        // Сравниваем версии
+        if ($githubPackageJson && $localPackageJson && isset($githubPackageJson['version']) && isset($localPackageJson['version'])) {
+            $githubVersion = $githubPackageJson['version'];
+            $localVersion = $localPackageJson['version'];
+
+            if (version_compare($githubVersion, $localVersion, '>')) {
+                echo 'Есть обновление! Текущая версия: ' . $localVersion . ', Новая версия: ' . $githubVersion;
+                // Добавьте здесь код для уведомления пользователя о необходимости обновления
+            } else {
+                echo 'У вас актуальная версия: ' . $localVersion;
+            }
         } else {
             echo 'Не удалось извлечь версию из package.json';
         }
     } else {
-        echo 'Не удалось получить содержимое файла package.json';
+        echo 'Не удалось получить содержимое файла package.json с GitHub';
     }
-    
-
-    // Остальной код для обработки запроса или вывода результатов
-    // ...
-
 } else {
     echo 'Неверный запрос';
 }
